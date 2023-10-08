29209236ceb0c61a20b2edad603b1ce6dcbbcc8a
macmiller87 <macamagolf@gmail.com>
2023-10-08 20:30:47 -0300

    Feat: configs credentials bd Postgres

---
+++ src/database/dbPostgres.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import postgres from 'postgres'; 
+
+const postgresSql = postgres({
+    host: process.env.POSTGRES_GHOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DATABASE,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+});
+
+export default postgresSql;
